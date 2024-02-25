076e2e283e1ab56f162e15605ac772a977007bf4
ddavilazari <ddavilazari@berklee.edu>
2024-02-24 19:20:46 -0500

    2d character 3

---
+++ EP391_V2/Assets/Script/PlayerController.cs
@@ -0,0 +1,21 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody rb;
+    public Transform head;
+    public Camera camera;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
